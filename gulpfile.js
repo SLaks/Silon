af7989e52ce3e3440c7868643eb87362af4d372b
SLaks <Dev@SLaks.net>
2015-05-12 09:34:10 -0400

    Use gulp-replace to kill selector duplication
    If an input appears multiple times in leaf nodes below an
    operator other than OR & NAND (which split their operands
    across commas), the generated selector will duplicate the
    inputs, preventing it from matching anything.  This regex
    removes the extra duplicate parts.
    
    This leaves some fully-duplicated selectors, which I will
    remove later.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var replace = require('gulp-replace');
 
 var slowPattern = './styles/chained-adder.less';
 
@@ -20,10 +21,18 @@ gulp.task('fast', function () {
 
 // chained-adder.less is slow enough that it should get a separate task,
 // so that changes to other files don't need to wait for it. It also has
-// no need for a source map; the mapping conveys no useful information.
+// no need for any source map; the mapping conveys no useful information
+// (omitting the source map also lets me skip running this when changing
+// other lines of _theme.less, and avoids breaking replace()).
 gulp.task('slow', function () {
 	return gulp.src(slowPattern)
 		.pipe(less())
 		.pipe(autoprefixer())
+		// If an input appears multiple times in leaf nodes below an
+		// operator other than OR & NAND (which split their operands
+		// across commas), the generated selector will duplicate the
+		// inputs, preventing it from matching anything.  This regex
+		// removes the extra duplicate parts.
+		.pipe(replace(/(^|\n|~ )([^~,]+)( ~[^,{]*)? ~ \2/g, '$1$2$3'))
 		.pipe(gulp.dest('./styles'));
 });
