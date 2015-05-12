9c36935f388ddbe86e717a5c584d11f5948d2379
SLaks <Dev@SLaks.net>
2015-05-12 09:05:18 -0400

    Split gulpfile into fast & slow tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,23 @@ var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function () {
-	return gulp.src('./styles/[^_]*.less')
+var slowPattern = './styles/chained-adder.less';
+
+gulp.task('fast', function () {
+	return gulp.src(['./styles/[^_]*.less', '!' + slowPattern])
 		.pipe(sourcemaps.init())
-		.pipe(less({
-			paths: [path.join(__dirname, 'less', 'includes')]
-		}))
+		.pipe(less())
 		.pipe(autoprefixer())
 		.pipe(sourcemaps.write('./', { includeContent: false }))
+		.pipe(gulp.dest('./styles'));
+});
+
+// chained-adder.less is slow enough that it should get a separate task,
+// so that changes to other files don't need to wait for it. It also has
+// no need for a source map; the mapping conveys no useful information.
+gulp.task('slow', function () {
+	return gulp.src(slowPattern)
+		.pipe(less())
+		.pipe(autoprefixer())
 		.pipe(gulp.dest('./styles'));
 });
